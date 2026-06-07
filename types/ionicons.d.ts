f2c28c645850ea643ca225bf51eb87c17f22e77f
Abbas Uddin <105127468+CodeAbbas@users.noreply.github.com>
2026-06-07 15:50:23 +0100

    Add TypeScript definitions for ion-icon component

---
+++ types/ionicons.d.ts
@@ -0,0 +1,16 @@
+import React from 'react';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'ion-icon': React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement> & {
+          name?: string;
+          class?: string;
+          size?: string;
+        },
+        HTMLElement
+      >;
+    }
+  }
+}
